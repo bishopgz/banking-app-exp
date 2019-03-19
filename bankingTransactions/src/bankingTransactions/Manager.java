7ad39c931950ccf3d144e0662505c76b8100c2f0
George Zafiris <zaf.mitx@gmail.com>
2019-03-19 23:46:59 +0200

    Add address and city modifications in details

--- bankingTransactions/src/bankingTransactions/Manager.java
+++ bankingTransactions/src/bankingTransactions/Manager.java
@@ -15,7 +15,7 @@ public class Manager {
 	public static void main(String[] args) {
 		
 		
-		JLabel masterLabel, balanceLabel, nameLabel,
+		JLabel masterLabel, balanceLabel, nameLabel, addressLabel, cityLabel,
 			   accountNum, relatedAccs, lastnameLabel;
 		
 		JTextField entry;
@@ -27,7 +27,7 @@ public class Manager {
 		
 		JFrame frame = new JFrame("Basic Account Management");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridLayout(5,1));
+		frame.setLayout(new GridLayout(6,1));
 		frame.setSize(400, 200);
 		
 		masterLabel = new JLabel("Deposit/Withdraw");
@@ -41,11 +41,13 @@ public class Manager {
 		relatedAccs = new JLabel("Connected accounts: " + customer.getRelatedAccounts());
 		nameLabel = new JLabel("Name: " + customer.getFirstname());
 		lastnameLabel = new JLabel("Lastname: " + customer.getLastname());
+		addressLabel = new JLabel("Address: " + customer.getAddress());
+		cityLabel = new JLabel("City: " + customer.getCity());
 		
 		changeAccountNumber.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String choice = JOptionPane.showInputDialog("Type 1 for name, 2 for lastname");
+				String choice = JOptionPane.showInputDialog("Type 1 for name, 2 for lastname, 3 for address, 4 for city");
 				if (isNumeric(choice)){
 					int c = Integer.parseInt(choice);
 					if (c == 1){
@@ -58,6 +60,17 @@ public class Manager {
 						customer.setLastname(last);
 						JOptionPane.showMessageDialog(null, "Lastname changed successfully");
 						lastnameLabel.setText(customer.getLastname());
+					} else if (c == 3){
+						String addr = JOptionPane.showInputDialog("Choose a new address");
+						customer.setAddress(addr);
+						JOptionPane.showMessageDialog(null, "Address changed successfully");
+						addressLabel.setText(customer.getAddress());						
+					} else if (c == 4){
+						String cty = JOptionPane.showInputDialog("Choose a new city");
+						customer.setCity(cty);
+						JOptionPane.showMessageDialog(null, "City changed successfully");
+						cityLabel.setText(customer.getCity());
+						
 					} else {
 						JOptionPane.showMessageDialog(null, "Not able to process this request.");
 						return;
@@ -115,6 +128,9 @@ public class Manager {
 		frame.add(lastnameLabel); // row 4, pos 2
 		frame.add(accountNum); // row 5, pos 1
 		frame.add(changeAccountNumber); // row 5, pos 2
+		frame.add(addressLabel); // row 6, pos 1
+		frame.add(cityLabel); // row 6, pos 2
+		
 		frame.setVisible(true);
 		
 
